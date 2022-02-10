45298427e4e8299703f32e05961bef3d15b7d668
Foo-x <www.sand.x@gmail.com>
2022-02-11 00:33:15 +0900

    refactor: remove unused react

--- __tests__/src/Tea.test.tsx
+++ __tests__/src/Tea.test.tsx
@@ -3,7 +3,6 @@ import { Sub } from '@/Sub';
 import { Tea, WithViewProps } from '@/Tea';
 import { Init, Update } from '@/useTea';
 import { render, screen } from '@testing-library/react';
-import React from 'react';
 import { act } from 'react-dom/test-utils';
 
 type Model = number;

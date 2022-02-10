84beed112e7620fc1e1f64fc2ffbc8df5a3b64b9
Foo-x <www.sand.x@gmail.com>
2022-02-11 01:31:42 +0900

    feat: not to export cmd functions

--- src/Cmd.ts
+++ src/Cmd.ts
@@ -4,19 +4,19 @@ export type Action<Msg> = (dispatch: Dispatch<Msg>) => void;
 export type PromiseAction<Msg> = (dispatch: Dispatch<Msg>) => Promise<void>;
 export type Cmd<Msg> = Array<Action<Msg> | PromiseAction<Msg>>;
 
-export const none = <Msg>(): Cmd<Msg> => {
+const none = <Msg>(): Cmd<Msg> => {
   return [];
 };
 
-export const delay = <Msg>(action: Action<Msg>, timeout: number): Cmd<Msg> => {
+const delay = <Msg>(action: Action<Msg>, timeout: number): Cmd<Msg> => {
   return [(dispatch) => setTimeout(() => action(dispatch), timeout)];
 };
 
-export const promise = <Msg>(promiseAction: PromiseAction<Msg>): Cmd<Msg> => {
+const promise = <Msg>(promiseAction: PromiseAction<Msg>): Cmd<Msg> => {
   return [(dispatch) => promiseAction(dispatch)];
 };
 
-export const batch = <Msg>(...cmds: Cmd<Msg>[]): Cmd<Msg> => {
+const batch = <Msg>(...cmds: Cmd<Msg>[]): Cmd<Msg> => {
   return cmds.flat();
 };
 
